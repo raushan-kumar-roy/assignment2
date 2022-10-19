5d518a81221fa68897003ec7b248574349a936b4
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-19 15:27:42 +0530

    stopwatch

---
+++ stopwatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2
+{
+    public class stopwatch
+    {
+        public static void Stopwatch()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            // Begin timing
+            stopwatch.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("HI");
+            }
+            // Stop timing
+            stopwatch.Stop();
+            Console.WriteLine("Time Elapsed : {0}",
+                stopwatch.Elapsed);
+            Console.ReadLine();
+        }
+    }
+
+}
+   
+    
