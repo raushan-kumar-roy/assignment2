d5a0ab8ff48999b55b0a7e6d9a3a4d3df412c3c0
raushan-kumar-roy <thorbtc0101@gmail.com>
2022-10-19 15:02:50 +0530

    assignment 2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using assignment_2;
+
+namespace classignment_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //fibonacci.getfibonacci();
+            //perfectNo.checkPerfectNo( );
+            //primeNo.checkPrime();
+            //reverse.getReverse();
+            stopwatch.Stopwatch();
+        }
+    }
+}
