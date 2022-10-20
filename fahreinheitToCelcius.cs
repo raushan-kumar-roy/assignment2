a31dbd8876fb6507eee6f1c6eb5ef97c780aecd9
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 19:39:09 +0530

    conversion

---
+++ fahreinheitToCelcius.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2
+{
+    public static class fahreinheitToCelcius
+    {
+        public static void Conversion()
+        {
+            Console.WriteLine("\nEnter your option:\n1.Celsius To Fahrenheit\n2.Fahrenheit To Celsius");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter value in Celsius:");
+                    double celsius = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Celsius To Fahrenheit: " + Utility.CelsiusToFahrenheit(celsius));
+                    break;
+                case 2:
+                    Console.WriteLine("Enter value in Fahrenheit:");
+                    double fahrenheit = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Fahrenheit To Celsius: " + Utility.FahrenheitToCelsius(fahrenheit));
+                    break;
+                default:
+                    Console.WriteLine("Wrong option!!!");
+                    break;
+            }
+        }
+    
+}
+}
