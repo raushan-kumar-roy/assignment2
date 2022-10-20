040e4014ac09efbfc53109d7993753e3bf508b2e
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 19:38:44 +0530

    daysofWeek

---
+++ daysOfWeek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2
+{
+    public static class daysOfWeek
+    {
+        public static void Days(int month, int day, int year)
+        {
+
+
+
+            string[] days = { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" };
+
+            int dayIndex = Utility.Days(month, day, year);
+
+            Console.WriteLine("days= " + days[dayIndex]);
+        }
+
+    
+}
+}
