1ce1a12fa4f83d53fe030973cae2113b3606ce4e
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 19:39:35 +0530

    monthlyPayment

---
+++ monthlyPayment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2
+{
+    public static class monthlyPayment
+    {
+        public static void Payment(string argument1, string argument2, string argument3)
+        {
+            double principalLoan = Convert.ToDouble(argument1);
+            double year = Convert.ToDouble(argument2);
+            double percentInterest = Convert.ToDouble(argument3);
+            Console.WriteLine("Monthly Payments: " + Utility.MonthlyPayment(principalLoan, year, percentInterest));
+        }
+        }
+    }
+}
