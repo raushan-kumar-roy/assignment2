b370854ce6ef21405179a6b00e1a0fd5b32c072d
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 19:40:26 +0530

    vendingMachine

---
+++ vendingMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2
+{
+    public static class vendingMachine
+    {
+        public static void Calculate()
+        {
+            int money = 2022;
+            int[] denominations = new int[] { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int note = 0;
+
+            while (money > 0)
+            {
+
+                int count = 0;
+
+                Console.WriteLine("Remaining money: " + money);
+
+                while (money >= denominations[note])
+                {
+
+                    money = money - denominations[note];
+
+                    count++;
+
+                }
+
+                Console.WriteLine(denominations[note] + " X " + count);
+
+                note++;
+
+
+
+            }
+        }
+    }
+}
