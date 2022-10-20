b8956aed23c695f6c668c92aa3eec09db99b4bad
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 19:40:49 +0530

    outputs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using assignment_2;
+
+namespace classignment_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //fibonacci.getfibonacci();
+            //perfectNo.checkPerfectNo( );
+            //primeNo.checkPrime();
+            //reverse.getReverse();
+            //stopwatch.Stopwatch();
+            //Coupen.CheckNumber();
+            //daysOfWeek.days();
+            //fahreinheitToCelcius.Conversion();
+            //vendingMachine.Calculate();
+            //utility.days();
+
+            //MonthlyPayment.Payment(args[ 3], args[ 4], args[5]);
+            //monthlyPayment.Payment();
+        }
+    }
+}
